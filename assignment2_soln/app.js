8f060bf4b33b82a651d81d42733a79dbd2bc3cb1
Nitesh Kanojia <niteshkanojia1010@gmail.com>
2023-06-20 14:06:35 +0530

    soln 4

--- assignment2_soln/app.js
+++ assignment2_soln/app.js
@@ -6,3 +6,16 @@ function myFunction() {
 		x.style.display = "block";
 	}
 }
+
+
+const names = ["John", "joker", "Nitesh", "jasmin", "rahul", "deepak", "kriti", "Jhonny", "kartik"];
+
+for (i = 0; i < names.length; i++) {
+	if (names[i].charAt(0) == 'J' || names[i].charAt(0) == 'j') {
+		console.log("Goodbye " + names[i])
+	}
+	else {
+		console.log("Hello " + names[i]);
+	}
+
+}
